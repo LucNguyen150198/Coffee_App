9b1354b93b2cd75403e2103ca7a4b0fb4375fb6e
Luc Nguyen <luc.nguyen@levincigroup.com>
2021-09-03 17:53:34 +0700

    custom component

---
+++ app/components/CustomButton.js
@@ -0,0 +1,121 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import { Colors, FontStyle, Layout, RADIUS, SPACING, w } from '../constants';
+import TouchableScale from 'react-native-touchable-scale';
+
+const BUTTON_WIDTH = w * 0.9;
+const BUTTON_HEIGHT = BUTTON_WIDTH * 0.14;
+
+export const CustomButton = ({
+  label = '',
+  width = BUTTON_WIDTH,
+  height = BUTTON_HEIGHT,
+  backgroundColor = Colors.primary,
+  labelColor = Colors.white,
+  style,
+  onPress,
+  iconName,
+}) => {
+  const handleOnPress = () => {
+    if (typeof onPress === 'function') {
+      onPress();
+    }
+  };
+
+  return (
+    <TouchableScale activeScale={0.8} friction={8} onPress={handleOnPress}>
+      <View
+        style={[
+          styles.container,
+          {
+            width,
+            height,
+            backgroundColor,
+          },
+          style,
+        ]}
+      >
+        <View style={[Layout.fill, Layout.rowCenter]}>
+          {iconName && (
+            <Image
+              source={iconName}
+              style={[styles.icon, { tintColor: labelColor }]}
+            />
+          )}
+          <Text
+            numberOfLines={1}
+            adjustsFontSizeToFit
+            style={[
+              styles.label,
+              {
+                color: labelColor,
+              },
+            ]}
+          >
+            {label}
+          </Text>
+        </View>
+      </View>
+    </TouchableScale>
+  );
+};
+
+export const IconButton = ({
+  size = 40,
+  backgroundColor = Colors.orange,
+  style,
+  onPress,
+  iconName,
+  tintColor = Colors.white,
+  border = RADIUS,
+}) => {
+  const handleOnPress = () => {
+    if (typeof onPress === 'function') {
+      onPress();
+    }
+  };
+
+  return (
+    <TouchableOpacity activeOpacity={0.8} onPress={handleOnPress}>
+      <View
+        style={[
+          styles.container,
+          {
+            width: size,
+            height: size,
+            backgroundColor,
+            borderRadius: border,
+          },
+          style,
+        ]}
+      >
+        <Image
+          source={iconName}
+          style={{
+            tintColor,
+            width: size / 3,
+            height: size / 3,
+            resizeMode: 'contain',
+          }}
+        />
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    ...Layout.center,
+    padding: SPACING,
+    borderRadius: RADIUS + 6,
+  },
+  label: {
+    ...FontStyle.h3,
+  },
+  icon: {
+    width: 20,
+    height: 20,
+    resizeMode: 'contain',
+    marginRight: SPACING,
+  },
+});
--- app/screens/Menu.js
+++ app/screens/Menu.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { CustomButton, IconButton } from '../components';
+import { Icons } from '../constants';
 export const Menu = () => {
   return (
-    <View>
-      <Text></Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      {/* <CustomButton label='ADD' iconName={Icons.menu_tab}/>
+      <IconButton iconName={Icons.menu_tab}/> */}
     </View>
   );
 };
